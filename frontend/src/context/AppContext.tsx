750c4aea42dbfeb711a314f987b87f97a732b2a4
punarvasu0311 <harshithk.0311@gmail.com>
2026-06-29 16:32:20 +0530

    App Contect added

---
+++ frontend/src/context/AppContext.tsx
@@ -0,0 +1,77 @@
+import axios from "axios";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  type ReactNode,
+} from "react";
+
+import { authService} from "../main";
+import { Toaster } from "react-hot-toast";
+import type { AppContextType, User } from "../types";
+
+
+const AppContext = createContext<AppContextType | undefined>(undefined);
+
+interface AppProviderProps {
+  children: ReactNode;
+}
+
+export const AppProvider = ({ children }: AppProviderProps) => {
+    const [user, setUser] = useState<User | null>(null);
+  const [isAuth, setIsAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const [location, setLocation] = useState(null);
+  const [loadingLocation, setLoadingLocation] = useState(false);
+  const [city, setCity] = useState("Fecthing Location...");
+
+  async function fetchUser() {
+    try {
+      const token = localStorage.getItem("token");
+
+      const { data } = await axios.get(`${authService}/api/auth/me`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setUser(data);
+      setIsAuth(true);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  return (
+    <AppContext.Provider
+      value={{
+        isAuth,
+        loading,
+        setIsAuth,
+        setLoading,
+        setUser,
+        user
+      }}
+    >
+      {children}
+
+      <Toaster />
+    </AppContext.Provider>
+  );
+};
+
+export const useAppData = (): AppContextType => {
+  const context = useContext(AppContext);
+  if (!context) {
+    throw new Error("useAppData must be used within AppProvider");
+  }
+  return context;
+};
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { AppProvider } from "./context/AppContext.tsx";
 
 export const authService = "http://localhost:5001";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
   <GoogleOAuthProvider clientId="599552265988-3u4lkshnjqoo3hqi5vjnn32m5h996146.apps.googleusercontent.com">
-    <App />
+    <AppProvider> //User contex is added
+      <App />
+    </AppProvider>
   </GoogleOAuthProvider>
   </StrictMode>
 )
