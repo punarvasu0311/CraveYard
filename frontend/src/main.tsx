a8b7ddfe1f9139bf3a6fd8fce601af88d9575d74
punarvasu0311 <harshithk.0311@gmail.com>
2026-06-29 15:11:12 +0530

    OAuth 2.0 connections in frontend

---
+++ frontend/src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+export const authService = "http://localhost:5001";
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+  <GoogleOAuthProvider clientId="599552265988-3u4lkshnjqoo3hqi5vjnn32m5h996146.apps.googleusercontent.com">
+    <App />
+  </GoogleOAuthProvider>
+  </StrictMode>
+)
