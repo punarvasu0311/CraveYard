6c250204b120b3d8de137d5ceb70f5a351f326a4
punarvasu0311 <harshithk.0311@gmail.com>
2026-06-29 16:32:54 +0530

    public and protected routes created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Home from "./pages/Home";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import {Toaster} from "react-hot-toast"
+import PublicRoute from "./components/publicRoute";
+import ProtectedRoute from "./components/protectedRoute";
+import SelectRole from "./pages/SelectRole";
+
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-        {/* <Route path='/' element={<Home />}/> */}
-        <Route path='/login' element={<Login />}/>
+          <Route element={<PublicRoute />}>
+            <Route path="/login" element={<Login />} />
+          </Route>
+          <Route element={<ProtectedRoute />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/select-role" element={<SelectRole />} />
+            
+          </Route>
         </Routes>
-        <Toaster/>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App
