6c6ff436388b943aac90651b5b37e4a29e917d22
punarvasu0311 <harshithk.0311@gmail.com>
2026-06-30 13:14:31 +0530

    adding comment technique

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,7 +10,7 @@ export const authService = "http://localhost:5001";
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
   <GoogleOAuthProvider clientId="599552265988-3u4lkshnjqoo3hqi5vjnn32m5h996146.apps.googleusercontent.com">
-    <AppProvider> //User contex is added
+    <AppProvider> {/* User Context is added*/}
       <App />
     </AppProvider>
   </GoogleOAuthProvider>
