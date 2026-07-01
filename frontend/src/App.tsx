f56e32030cd51fc72d747e5b7d1b619195cfc5a4
punarvasu0311 <harshithk.0311@gmail.com>
2026-07-01 23:53:32 +0530

    restaurant added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,9 +6,20 @@ import ProtectedRoute from "./components/protectedRoute";
 import SelectRole from "./pages/SelectRole";
 import Navbar from "./components/navbar";
 import Account from "./pages/Account";
+import { useAppData } from "./context/AppContext";
+import Restaurant from "./pages/Restaurant";
 
 
 const App = () => {
+  const { user, loading } = useAppData();
+
+  if (loading) {
+    return (
+      <h1 className="text-2xl font-bold text-red-500 text-center mt-56">
+        Loading...
+      </h1>
+    );
+  }
   return (
     <>
       <BrowserRouter>
