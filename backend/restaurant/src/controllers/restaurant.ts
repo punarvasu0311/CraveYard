d3c79ea4fc0a9104cd5e542328de3722a4aa7bd7
punarvasu0311 <harshithk.0311@gmail.com>
2026-07-01 23:55:02 +0530

    1st commit

---
+++ backend/restaurant/src/controllers/restaurant.ts
@@ -0,0 +1,97 @@
+import axios from "axios";
+import { AuthenticatedRequest } from "../middlewares/isAuth.js";
+import getBuffer from "../config/datauri.js";
+import TryCatch from "../middlewares/trycatch.js";
+import Restaurant from "../models/Restaurant.js";
+import jwt from "jsonwebtoken";
+
+export const addRestaurant = TryCatch(async (req: AuthenticatedRequest, res) => {
+  const user = req.user;
+
+  if (!user) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+
+  const existingRestaunrant = await Restaurant.findOne({
+    ownerId: user._id,
+  });
+
+  if (existingRestaunrant) {
+    return res.status(400).json({
+      message: "You already have a restaurant",
+    });
+  }
+  // latitude and longitude is recieved from frontend via AppContext.tsx
+  const { name, description, latitude, longitude, formattedAddress, phone } =
+    req.body;
+
+  if (!name || !latitude || !longitude) {
+    return res.status(400).json({
+      message: "Please give all details",
+    });
+  }
+
+  const file = req.file;
+
+  if (!file) {
+    return res.status(400).json({
+      message: "Please give image",
+    });
+  }
+  // files is sent to datauri
+  const fileBuffer = getBuffer(file);
+
+  if (!fileBuffer?.content) {
+    return res.status(500).json({
+      message: "Failed to create file buffer",
+    });
+  }
+
+  const { data: uploadResult } = await axios.post(
+    `${process.env.UTILS_SERVICE}/api/upload`,
+    {
+      buffer: fileBuffer.content,
+    }
+  );
+
+  const restaurant = await Restaurant.create({
+    name,
+    description,
+    phone,
+    image: uploadResult.url,
+    ownerId: user._id,
+    autoLocation: {
+      type: "Point",
+      coordinates: [Number(longitude), Number(latitude)],
+      formattedAddress,
+    },
+    isVerified: false,
+  });
+
+  return res.status(201).json({
+    message: "Restaurant created successfully",
+    restaurant,
+  });
+});
+
+export const fetchMyRestaurant = TryCatch(
+  async (req: AuthenticatedRequest, res) => {
+    if (!req.user) {
+      return res.status(401).json({
+        message: "Please Login",
+      });
+    }
+    const restaurant = await Restaurant.findOne({ ownerId: req.user._id });
+
+    if (!restaurant) {
+      return res.status(400).json({
+        message: "No Restaurant found",
+      });
+    }
+    //used for real time orders when connected to socket.io
+
+    res.json({ restaurant });
+  }
+);
