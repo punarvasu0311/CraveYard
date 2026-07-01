eb382d87010cf914b9ae38b313c88b4fb8ff1967
punarvasu0311 <harshithk.0311@gmail.com>
2026-07-01 23:54:16 +0530

    restaurant route added

--- backend/restaurant/src/index.ts
+++ backend/restaurant/src/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import restaurantRoutes from "./routes/restaurant.js";
 
 dotenv.config()
 
 const app = express();
 
 const PORT = process.env.PORT || 5002;
+
+app.use("/api/restaurant",restaurantRoutes)
 
 
 app.listen(PORT, () => {
