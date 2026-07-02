9f0e66a9952d8756a31637271ed5d65e6659bd00
punarvasu0311 <harshithk.0311@gmail.com>
2026-07-02 19:27:48 +0530

    cors added

--- backend/restaurant/src/index.ts
+++ backend/restaurant/src/index.ts
@@ -8,7 +8,7 @@ dotenv.config()
 
 const app = express();
 app.use(cors());
-app.use(express.json);
+app.use(express.json());
 
 const PORT = process.env.PORT || 5002;
 
@@ -17,5 +17,5 @@ app.use("/api/restaurant",restaurantRoutes)
 
 app.listen(PORT, () => {
   console.log(`Restaurant service is running on port ${PORT}`);
-  connectDB
+  connectDB();
 });
