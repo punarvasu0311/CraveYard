b890550f802a319278f8ba5a012cd633407fe814
punarvasu0311 <harshithk.0311@gmail.com>
2026-07-02 16:17:31 +0530

    seller only view point added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import { AppProvider } from "./context/AppContext.tsx";
 
 export const authService = "http://localhost:5001";
+export const restaurantService = "http://localhost:5002";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
