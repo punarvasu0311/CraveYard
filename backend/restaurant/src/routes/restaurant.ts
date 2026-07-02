a4582baead3ee86d6716ab993b263b9c76f58d7f
punarvasu0311 <harshithk.0311@gmail.com>
2026-07-02 21:12:47 +0530

    toggle,edit route added

--- backend/restaurant/src/routes/restaurant.ts
+++ backend/restaurant/src/routes/restaurant.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { isAuth, isSeller } from "../middlewares/isAuth.js";
 import uploadFile from "../middlewares/multer.js";
-import { addRestaurant,fetchMyRestaurant } from "../controllers/restaurant.js";
+import { addRestaurant,fetchMyRestaurant,updateStatusRestaurant,updateRestaurant } from "../controllers/restaurant.js";
 
 
 const router = express.Router();
 router.post("/new", isAuth, isSeller, uploadFile, addRestaurant);
 router.get("/my", isAuth, isSeller, fetchMyRestaurant);
-
+router.put("/status",isAuth,isSeller,updateStatusRestaurant)
+router.put("/edit",isAuth,isSeller,updateRestaurant)
 export default router;
