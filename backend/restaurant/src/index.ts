c435a01cc4beaa9b64cac0fdaf17736a628e704e
punarvasu0311 <harshithk.0311@gmail.com>
2026-07-15 13:20:34 +0530

    address logic

--- backend/restaurant/src/index.ts
+++ backend/restaurant/src/index.ts
@@ -5,6 +5,7 @@ import restaurantRoutes from "./routes/restaurant.js";
 import cors from "cors";
 import itemRoutes from "./routes/menuitem.js"
 import cartRoutes from "./routes/cart.js";
+import addressRoutes from "./routes/address.js";
 
 dotenv.config()
 
@@ -17,6 +18,7 @@ const PORT = process.env.PORT || 5002;
 app.use("/api/restaurant",restaurantRoutes);
 app.use("/api/item", itemRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/address", addressRoutes);
 
 app.listen(PORT, () => {
   console.log(`Restaurant service is running on port ${PORT}`);
