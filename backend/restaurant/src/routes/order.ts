b886f3f7f51f52637fc4b81e67d85af81f0d35f9
punarvasu0311 <harshithk.0311@gmail.com>
2026-07-16 19:19:32 +0530

    1st commit

---
+++ backend/restaurant/src/routes/order.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { isAuth, isSeller } from "../middlewares/isAuth.js";
+import {
+
+  createOrder,
+  fetchOrderForPayment,
+  
+} from "../controllers/order.js";
+
+const router = express.Router();
+
+
+router.post("/new", isAuth, createOrder);
+router.get("/payment/:id", fetchOrderForPayment);
+
+
+export default router;
