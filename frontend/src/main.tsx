255dadb1e570309fd88f255cc9172d8753ff3174
punarvasu0311 <harshithk.0311@gmail.com>
2026-07-18 14:02:53 +0530

    SocketContext created

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,16 +5,20 @@ import App from './App.tsx'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { AppProvider } from "./context/AppContext.tsx";
 import "leaflet/dist/leaflet.css";
+import { SocketProvider } from './context/SocketContext.tsx';
 
 export const authService = "http://localhost:5001";
 export const restaurantService = "http://localhost:5002";
 export const utilsService = "http://localhost:5003";
+export const realtimeService = "http://localhost:5005";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
   <GoogleOAuthProvider clientId="599552265988-3u4lkshnjqoo3hqi5vjnn32m5h996146.apps.googleusercontent.com">
     <AppProvider> {/* User Context is added*/}
-      <App />
+      <SocketProvider>
+        <App />
+        </SocketProvider>
     </AppProvider>
   </GoogleOAuthProvider>
   </StrictMode>
