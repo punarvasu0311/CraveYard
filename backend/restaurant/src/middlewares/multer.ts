937f45bb092c790af5cf510dbf49b13a825c6e7b
punarvasu0311 <harshithk.0311@gmail.com>
2026-07-20 23:48:23 +0530

    explanation of multer is added

--- backend/restaurant/src/middlewares/multer.ts
+++ backend/restaurant/src/middlewares/multer.ts
@@ -1,7 +1,7 @@
 import multer from "multer";
 
 const storage = multer.memoryStorage();
-
+// this adds the part of file in frontend formdata(addRestaurant.tsx) to req.file and remaining to req.body
 const uploadFile = multer({ storage }).single("file");
 
 export default uploadFile;
